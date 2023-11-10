d3addb20dfe516aa6e881c7dd7d3e860ed7d5cfe
patelsweta2 <80040744+patelsweta2@users.noreply.github.com>
2023-11-10 19:50:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // your code here
+function updateUrl() {
+	const nameValue = document.getElementById('name').value;
+	const yearValue = document.getElementById('year').value;
+
+	const urlElement = document.getElementById('url');
+	urlElement.textContent = `https://localhost:8080/?name=${nameValue}&year=${yearValue}`;
+
+	return false;
+}
+
+const form = document.getElementById('queryStringForm');
+form.addEventListener('submit', function(e) {
+	e.preventDefault();
+	updateUrl();
+})
